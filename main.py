3170cfb2cc827b917fe13c86ec0ed62c2ddde892
praveennair <textpraveennair@gmail.com>
2020-10-06 16:13:03 +0530

    checker

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error Fetching: {res.status_code}, check the api and try again')
+
+
+
+request_api_data('123')
